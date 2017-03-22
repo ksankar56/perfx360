aa92cfe6cef946038449e48545f62007762eed8c
pensen <pensen@gmail.com>
2017-03-23 00:29:55 +0530

    New Configuration added

--- app.js
+++ app.js
@@ -1,31 +1,10 @@
 var express = require('express');
-var path = require('path');
 var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
-
-var index = require('./routes/index');
-var users = require('./routes/users');
-var about = require('./routes/about');
+var configure = require('./src/common/configure');
 
 var app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
-
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', index);
-app.use('/users', users);
-app.use('/about', about);
+configure.init(app);
 
 app.use("*",function(req,res){
     res.sendFile(path + "404.html");
@@ -43,7 +22,7 @@ app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
-  console.log("Live at Port 3000");
+
   // render the error page
   res.status(err.status || 500);
   res.render('error');
